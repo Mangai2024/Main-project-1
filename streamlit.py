de0a607af15079ddc9988d26ed789c01cf2ff8dd
Gowri Shankar K <carthishank80@gmail.com>
2024-12-25 19:26:17 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,25 +5,25 @@ import numpy as np
 import pickle
 import os
 
-#def get_db_connection():
-    #conn = psycopg2.connect(
-       # host="ec2-13-201-77-225.ap-south-1.compute.amazonaws.com",
-    #)
-   # return conn
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="ec2-13-201-77-225.ap-south-1.compute.amazonaws.com",
+    )
+    return conn
 
-# Function to execute a query and return the result as a pandas DataFrame
-#def run_query(query):
-    #conn = get_db_connection()
-    #if conn is None:
-        #return None
-    #try:
-        #df = pd.read_sql(query, conn)
-        #return df
-    #except Exception as e:
-        #st.error(f"Error executing query: {e}")
-        #return None
-    #finally:
-        #conn.close()
+ #Function to execute a query and return the result as a pandas DataFrame
+def run_query(query):
+    conn = get_db_connection()
+    if conn is None:
+        return None
+    try:
+        df = pd.read_sql(query, conn)
+        return df
+    except Exception as e:
+        st.error(f"Error executing query: {e}")
+        return None
+    finally:
+        conn.close()
 
 # Function to load a model from a specified file path
 def load_model(file_path):
